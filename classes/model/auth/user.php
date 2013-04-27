8ac0b378a17489e83c8216af02f7e0e3e166e58d
WanWizard <wanwizard@wanwizard.eu>
2013-04-27 21:25:01 +0200

    improved Model\Auth_User default properties definition

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -29,46 +29,55 @@ class Auth_User extends \Orm\Model
 	 */
 	protected static $_properties = array(
 		'id',
-		'username'     => array(
+		'username'        => array(
 			'label'		  => 'auth_model_user.name',
 			'default' 	  => 0,
 			'null'		  => false,
 			'validation'  => array('required', 'max_length' => array(255))
 		),
-		'password'     => array(
-			'label'		  => 'auth_model_user.password',
+		'email'           => array(
+			'label'		  => 'auth_model_user.email',
 			'default' 	  => 0,
 			'null'		  => false,
-			'form'  	  => array('type' => 'password'),
-			'validation'  => array('min_length' => array(8), 'match_field' => array('confirm'))
+			'validation'  => array('required', 'valid_email')
 		),
-		'group_id'     => array(
+		'group_id'        => array(
 			'label'		  => 'auth_model_user.group_id',
 			'default' 	  => 0,
 			'null'		  => false,
 			'form'  	  => array('type' => 'select'),
 			'validation'  => array('required', 'is_numeric')
 		),
-		'email'     => array(
-			'label'		  => 'auth_model_user.email',
+		'password'        => array(
+			'label'		  => 'auth_model_user.password',
 			'default' 	  => 0,
 			'null'		  => false,
-			'validation'  => array('required', 'valid_email')
+			'form'  	  => array('type' => 'password'),
+			'validation'  => array('min_length' => array(8), 'match_field' => array('confirm'))
 		),
-		'last_login',
-		'previous_login',
-		'login_hash',
+		'last_login'	  => array(
+			'form'  	  => array('type' => false),
+		),
+		'previous_login'  => array(
+			'form'  	  => array('type' => false),
+		),
+		'login_hash'	  => array(
+			'form'  	  => array('type' => false),
+		),
 		'user_id'         => array(
 			'default' 	  => 0,
 			'null'		  => false,
+			'form'  	  => array('type' => false),
 		),
 		'created_at'      => array(
 			'default' 	  => 0,
 			'null'		  => false,
+			'form'  	  => array('type' => false),
 		),
 		'updated_at'      => array(
 			'default' 	  => 0,
 			'null'		  => false,
+			'form'  	  => array('type' => false),
 		),
 	);
 
