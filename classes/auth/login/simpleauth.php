2afa8c4e82bca6a0149fe41cb8688ae5c79a617c
WanWizard <wanwizard@wanwizard.eu>
2013-06-13 10:40:42 +0200

    fixed typo in Simpleauth::create_user()

--- classes/auth/login/simpleauth.php
+++ classes/auth/login/simpleauth.php
@@ -254,7 +254,7 @@ class Auth_Login_Simpleauth extends \Auth_Login_Driver
 			'username'        => (string) $username,
 			'password'        => $this->hash_password((string) $password),
 			'email'           => $email,
-			'group_id'        => (int) $group,
+			'group'           => (int) $group,
 			'profile_fields'  => serialize($profile_fields),
 			'last_login'      => 0,
 			'login_hash'      => '',
