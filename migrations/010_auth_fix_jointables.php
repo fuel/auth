26dc3926117c8cdbb441a9c857e2d04c973f7f97
WanWizard <wanwizard@wanwizard.eu>
2013-11-24 23:25:21 +0100

    fixed index issues when added a new PK

--- migrations/010_auth_fix_jointables.php
+++ migrations/010_auth_fix_jointables.php
@@ -22,14 +22,19 @@ class Auth_Fix_Jointables
 			// make sure the configured DB is used
 			\DBUtil::set_connection(\Config::get('simpleauth.db_connection', null));
 
+			\DBUtil::drop_index($basetable.'_user_permissions', 'primary');
 			\DBUtil::add_fields($basetable.'_user_permissions', array(
-				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'first' => true),
+				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'primary_key' => true, 'first' => true),
 			));
+
+			\DBUtil::drop_index($basetable.'_group_permissions', 'primary');
 			\DBUtil::add_fields($basetable.'_group_permissions', array(
-				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'first' => true),
+				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'primary_key' => true, 'first' => true),
 			));
+
+			\DBUtil::drop_index($basetable.'_role_permissions', 'primary');
 			\DBUtil::add_fields($basetable.'_role_permissions', array(
-				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'first' => true),
+				'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true, 'primary_key' => true, 'first' => true),
 			));
 		}
 
@@ -54,15 +59,24 @@ class Auth_Fix_Jointables
 			// make sure the configured DB is used
 			\DBUtil::set_connection(\Config::get('ormauth.db_connection', null));
 
+			\DBUtil::drop_index($basetable.'_user_permissions', 'primary');
 			\DBUtil::drop_fields($basetable.'_user_permissions', array(
 				'id',
 			));
+			\DBUtil::create_index($basetable.'_user_permissions', array('user_id', 'perms_id'), '', 'PRIMARY');
+
+
+			\DBUtil::drop_index($basetable.'_group_permissions', 'primary');
 			\DBUtil::drop_fields($basetable.'_group_permissions', array(
 				'id',
 			));
+			\DBUtil::create_index($basetable.'_group_permissions', array('group_id', 'perms_id'), '', 'PRIMARY');
+
+			\DBUtil::drop_index($basetable.'_role_permissions', 'primary');
 			\DBUtil::drop_fields($basetable.'_role_permissions', array(
 				'id',
 			));
+			\DBUtil::create_index($basetable.'_role_permissions', array('role_id', 'perms_id'), '', 'PRIMARY');
 		}
 
 		// reset any DBUtil connection set
