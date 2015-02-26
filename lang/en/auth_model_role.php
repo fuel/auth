d240d77aaa0d1e0468118cf653c84fd8d706518c
Michiel Vugteveen <info@it-can.nl>
2015-02-26 22:55:49 +0100

    style fix

--- lang/en/auth_model_role.php
+++ lang/en/auth_model_role.php
@@ -1,13 +1,13 @@
 <?php
 
 return array(
-	'name'          => 'Role name',
-	'filter'        => 'Special permissions',
+    'name'   => 'Role name',
+    'filter' => 'Special permissions',
 
-	'permissions'   => array(
-		''          => 'None',
-		'A'         => 'Allow all access',
-		'D'         => 'Deny all access',
-		'R'         => 'Revoke assigned permissions',
+    'permissions' => array(
+        ''  => 'None',
+        'A' => 'Allow all access',
+        'D' => 'Deny all access',
+        'R' => 'Revoke assigned permissions',
 	),
 );
