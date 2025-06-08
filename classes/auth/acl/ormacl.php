a2729dd7e6433cf74ea9a145febd504640cc25e9
WanWizard <wanwizard@wanwizard.eu>
2025-06-08 14:10:47 +0100

    fixed broken action access check
    this reverts 7187cc53c5d15079ddf795dfa0a5f06d8a215b95
    
    thanks "bvn" for reporting this.

--- classes/auth/acl/ormacl.php
+++ classes/auth/acl/ormacl.php
@@ -154,10 +154,10 @@ class Auth_Acl_Ormacl extends \Auth_Acl_Driver
 							{
 								$revoked_rights[$permission->area][$permission->permission] = array_merge(
 									$revoked_rights[$permission->area][$permission->permission],
-									array_flip(array_intersect_key(
-										array_flip($permission->actions ?: array()),
+									array_intersect_key(
+										$permission->actions ?: array(),
 										array_flip($rolepermission->actions ?: array())
-									))
+									)
 								);
 								break;
 							}
@@ -178,10 +178,10 @@ class Auth_Acl_Ormacl extends \Auth_Acl_Driver
 							{
 								$current_rights[$permission->area][$permission->permission] = array_merge(
 									$current_rights[$permission->area][$permission->permission],
-									array_flip(array_intersect_key(
-										array_flip($permission->actions ?: array()),
+									array_intersect_key(
+										$permission->actions ?: array(),
 										array_flip($rolepermission->actions ?: array())
-									))
+									)
 								);
 								break;
 							}
@@ -207,10 +207,10 @@ class Auth_Acl_Ormacl extends \Auth_Acl_Driver
 								{
 									$current_rights[$permission->area][$permission->permission] = array_merge(
 										$current_rights[$permission->area][$permission->permission],
-										array_flip(array_intersect_key(
-											array_flip($permission->actions ?: array()),
+										array_intersect_key(
+											$permission->actions ?: array(),
 											array_flip($grouppermission->actions ?: array())
-										))
+										)
 									);
 									break;
 								}
@@ -228,10 +228,10 @@ class Auth_Acl_Ormacl extends \Auth_Acl_Driver
 							{
 								$current_rights[$permission->area][$permission->permission] = array_merge(
 									$current_rights[$permission->area][$permission->permission],
-									array_flip(array_intersect_key(
-										array_flip($permission->actions ?: array()),
+									array_intersect_key(
+										$permission->actions ?: array(),
 										array_flip($userpermission->actions ?: array())
-									))
+									)
 								);
 								break;
 							}
@@ -242,7 +242,7 @@ class Auth_Acl_Ormacl extends \Auth_Acl_Driver
 
 			// save the rights in the cache
 			$this->_acl_cache[$cache_key] = array($current_rights, $revoked_rights, $global_access);
-			\Cache::set($cache_key, $this->_acl_cache[$cache_key]);
+			// \Cache::set($cache_key, $this->_acl_cache[$cache_key]);
 		}
 
 		// check for a revocation first
